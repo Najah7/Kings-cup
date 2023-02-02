11af63487674917b1573d592278af0adc59833a3
Najah7 <najahmatsuo7@gmail.com>
2023-02-02 19:16:18 +0900

    ✨ トランプコンポーネント実装（第１回）

---
+++ kings-cup/src/app/components/Card/card.tsx
@@ -0,0 +1,46 @@
+import { style } from '@mui/system'
+import Image from 'next/image'
+import { useCallback, useEffect, useState } from 'react'
+import styles from './card.module.css'
+
+export const Card = () => {
+
+    const [isFront, setIsFront] = useState(false)
+
+    const ClickHandler = () => setIsFront((prevState) => !prevState)
+
+
+    return (
+        <>
+            <div className={styles.card}>
+                <div className={styles.inner}>
+                    {isFront?
+                        <div className={styles.front}>
+                        <Image
+                        src="/AceSpades.png"
+                        alt='表面'
+                        width={150}
+                        height={300}
+                        priority
+                        className={styles.img}
+                        onClick={ClickHandler}
+                        />
+                        </div>
+                    :
+                        <div className={styles.back}>
+                        <Image
+                        src="/card-back-Blue.png"
+                        alt='トランプ裏面'
+                        width={150}
+                        height={300}
+                        priority
+                        className={styles.img}
+                        onClick={ClickHandler}
+                        />
+                        </div>
+                    }
+                </div>
+            </div>
+        </>
+    )
+}
