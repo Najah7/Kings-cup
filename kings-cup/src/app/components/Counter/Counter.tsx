72432db97383edf507b2e19495d83c0a9051ea64
Najah7 <najahmatsuo7@gmail.com>
2023-02-02 19:18:02 +0900

    ✨ カウンターコンポーネント実装（第１回）

---
+++ kings-cup/src/app/components/Counter/Counter.tsx
@@ -0,0 +1,14 @@
+import styles from './Counter.module.css'
+
+// エラーを非表示にするためのany型
+export const Counter = (props:any) => {
+    
+
+    return (
+        <>
+        <div className={styles.container}>
+        <h2 className={styles.h2}>{props.title}&nbsp;<span className={styles.num}>{props.num}</span></h2>
+        </div>
+        </>
+    )
+}
