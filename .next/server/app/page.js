5f73d3f2e60eb38d51870410fbf8f7f28211d80a
Najah7 <najahmatsuo7@gmail.com>
2023-02-03 17:06:46 +0900

    🚀 buildコマンドを実行

---
+++ .next/server/app/page.js
@@ -0,0 +1,677 @@
+(() => {
+var exports = {};
+exports.id = 931;
+exports.ids = [931];
+exports.modules = {
+
+/***/ 18038:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/compiled/react");
+
+/***/ }),
+
+/***/ 98704:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/compiled/react-dom/server-rendering-stub");
+
+/***/ }),
+
+/***/ 97897:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/compiled/react-server-dom-webpack/client");
+
+/***/ }),
+
+/***/ 56786:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/compiled/react/jsx-runtime");
+
+/***/ }),
+
+/***/ 53918:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/amp-context.js");
+
+/***/ }),
+
+/***/ 45732:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/amp-mode.js");
+
+/***/ }),
+
+/***/ 3280:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/app-router-context.js");
+
+/***/ }),
+
+/***/ 92796:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/head-manager-context.js");
+
+/***/ }),
+
+/***/ 69274:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/hooks-client-context.js");
+
+/***/ }),
+
+/***/ 34014:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/i18n/normalize-locale-path.js");
+
+/***/ }),
+
+/***/ 64486:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/image-blur-svg.js");
+
+/***/ }),
+
+/***/ 50744:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/image-config-context.js");
+
+/***/ }),
+
+/***/ 35843:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/image-config.js");
+
+/***/ }),
+
+/***/ 99552:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/image-loader");
+
+/***/ }),
+
+/***/ 78524:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/is-plain-object.js");
+
+/***/ }),
+
+/***/ 78020:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/mitt.js");
+
+/***/ }),
+
+/***/ 47342:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/no-ssr-error.js");
+
+/***/ }),
+
+/***/ 64406:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/page-path/denormalize-page-path.js");
+
+/***/ }),
+
+/***/ 11751:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/add-path-prefix.js");
+
+/***/ }),
+
+/***/ 46220:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/compare-states.js");
+
+/***/ }),
+
+/***/ 10299:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/format-next-pathname-info.js");
+
+/***/ }),
+
+/***/ 23938:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/format-url.js");
+
+/***/ }),
+
+/***/ 29565:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/get-asset-path-from-route.js");
+
+/***/ }),
+
+/***/ 35789:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/get-next-pathname-info.js");
+
+/***/ }),
+
+/***/ 1897:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/is-bot.js");
+
+/***/ }),
+
+/***/ 1428:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/is-dynamic.js");
+
+/***/ }),
+
+/***/ 28854:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/parse-path.js");
+
+/***/ }),
+
+/***/ 91292:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/parse-relative-url.js");
+
+/***/ }),
+
+/***/ 34567:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/path-has-prefix.js");
+
+/***/ }),
+
+/***/ 80979:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/querystring.js");
+
+/***/ }),
+
+/***/ 93297:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/remove-trailing-slash.js");
+
+/***/ }),
+
+/***/ 36052:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/resolve-rewrites.js");
+
+/***/ }),
+
+/***/ 84226:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/route-matcher.js");
+
+/***/ }),
+
+/***/ 95052:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/route-regex.js");
+
+/***/ }),
+
+/***/ 3349:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/server-inserted-html.js");
+
+/***/ }),
+
+/***/ 82470:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/side-effect.js");
+
+/***/ }),
+
+/***/ 59232:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/utils.js");
+
+/***/ }),
+
+/***/ 40618:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/utils/warn-once.js");
+
+/***/ }),
+
+/***/ 84315:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"card": "king_card__8OGep",
+	"inner": "king_inner__yVL3r",
+	"img": "king_img__96Gyw"
+};
+
+
+/***/ }),
+
+/***/ 74577:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"table": "table_table__vnDpc"
+};
+
+
+/***/ }),
+
+/***/ 26279:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"h1": "title_h1__1_nfs"
+};
+
+
+/***/ }),
+
+/***/ 61159:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"app": "page_app__1ooRt",
+	"h2": "page_h2__VnyuY"
+};
+
+
+/***/ }),
+
+/***/ 75385:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "AppRouter": () => (/* reexport default from dynamic */ next_dist_client_components_app_router__WEBPACK_IMPORTED_MODULE_0___default.a),
+/* harmony export */   "GlobalError": () => (/* reexport default from dynamic */ next_dist_client_components_error_boundary__WEBPACK_IMPORTED_MODULE_3___default.a),
+/* harmony export */   "LayoutRouter": () => (/* reexport default from dynamic */ next_dist_client_components_layout_router__WEBPACK_IMPORTED_MODULE_1___default.a),
+/* harmony export */   "RenderFromTemplateContext": () => (/* reexport default from dynamic */ next_dist_client_components_render_from_template_context__WEBPACK_IMPORTED_MODULE_2___default.a),
+/* harmony export */   "__next_app_webpack_require__": () => (/* binding */ __next_app_webpack_require__),
+/* harmony export */   "metadata": () => (/* binding */ metadata),
+/* harmony export */   "pages": () => (/* binding */ pages),
+/* harmony export */   "renderToReadableStream": () => (/* reexport safe */ next_dist_compiled_react_server_dom_webpack_server_browser__WEBPACK_IMPORTED_MODULE_7__.renderToReadableStream),
+/* harmony export */   "requestAsyncStorage": () => (/* reexport safe */ next_dist_client_components_request_async_storage__WEBPACK_IMPORTED_MODULE_5__.requestAsyncStorage),
+/* harmony export */   "serverHooks": () => (/* reexport module object */ next_dist_client_components_hooks_server_context__WEBPACK_IMPORTED_MODULE_6__),
+/* harmony export */   "staticGenerationAsyncStorage": () => (/* reexport safe */ next_dist_client_components_static_generation_async_storage__WEBPACK_IMPORTED_MODULE_4__.staticGenerationAsyncStorage),
+/* harmony export */   "tree": () => (/* binding */ tree)
+/* harmony export */ });
+/* harmony import */ var next_dist_client_components_app_router__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(72315);
+/* harmony import */ var next_dist_client_components_app_router__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_app_router__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_dist_client_components_layout_router__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(62333);
+/* harmony import */ var next_dist_client_components_layout_router__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_layout_router__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_dist_client_components_render_from_template_context__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(62885);
+/* harmony import */ var next_dist_client_components_render_from_template_context__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_render_from_template_context__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var next_dist_client_components_error_boundary__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(19505);
+/* harmony import */ var next_dist_client_components_error_boundary__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_error_boundary__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var next_dist_client_components_static_generation_async_storage__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(90683);
+/* harmony import */ var next_dist_client_components_static_generation_async_storage__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_static_generation_async_storage__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var next_dist_client_components_request_async_storage__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(23269);
+/* harmony import */ var next_dist_client_components_request_async_storage__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_request_async_storage__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var next_dist_client_components_hooks_server_context__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(85746);
+/* harmony import */ var next_dist_client_components_hooks_server_context__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_hooks_server_context__WEBPACK_IMPORTED_MODULE_6__);
+/* harmony import */ var next_dist_compiled_react_server_dom_webpack_server_browser__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(8208);
+/* harmony import */ var next_dist_compiled_react_server_dom_webpack_server_browser__WEBPACK_IMPORTED_MODULE_7___default = /*#__PURE__*/__webpack_require__.n(next_dist_compiled_react_server_dom_webpack_server_browser__WEBPACK_IMPORTED_MODULE_7__);
+
+    const tree = {
+        children: [
+        '',
+        {
+        children: ['', {}, {
+          page: [() => Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 27306, 23)), "/home/najah/kings_cup/src/app/page.tsx"]}]
+      },
+        {
+          'layout': [() => Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 79907)), "/home/najah/kings_cup/src/app/layout.tsx"],
+        }
+      ]
+      }.children;
+    const metadata = [{
+          type: 'layout',
+          layer: 0,
+          mod: () => Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 79907)),
+          path: "/home/najah/kings_cup/src/app/layout.tsx",
+        },{
+          type: 'page',
+          layer: 0,
+          mod: () => Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 27306, 23)),
+          path: "/home/najah/kings_cup/src/app/page.tsx",
+        },];
+    const pages = ["/home/najah/kings_cup/src/app/page.tsx"];
+
+    
+    
+    
+    
+
+    
+    
+
+    
+
+    
+    const __next_app_webpack_require__ = __webpack_require__
+  
+
+/***/ }),
+
+/***/ 26878:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 89446, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 43258, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 56862, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 82030, 23))
+
+/***/ }),
+
+/***/ 4377:
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ 24464:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 99859))
+
+/***/ }),
+
+/***/ 79907:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ RootLayout)
+});
+
+// EXTERNAL MODULE: ./node_modules/next/dist/compiled/react/jsx-runtime.js
+var jsx_runtime = __webpack_require__(88499);
+// EXTERNAL MODULE: ./src/app/globals.css
+var globals = __webpack_require__(75553);
+;// CONCATENATED MODULE: ./src/app/components/Head/head.tsx
+
+function Head(props) {
+    return /*#__PURE__*/ (0,jsx_runtime.jsxs)(jsx_runtime.Fragment, {
+        children: [
+            /*#__PURE__*/ jsx_runtime.jsx("title", {
+                children: props.title
+            }),
+            /*#__PURE__*/ jsx_runtime.jsx("meta", {
+                content: "width=device-width, initial-scale=1",
+                name: "viewport"
+            }),
+            /*#__PURE__*/ jsx_runtime.jsx("meta", {
+                name: "description",
+                content: "Generated by create next app"
+            }),
+            /*#__PURE__*/ jsx_runtime.jsx("link", {
+                rel: "icon",
+                href: "/card-icon.svg"
+            })
+        ]
+    });
+}
+
+;// CONCATENATED MODULE: ./src/app/layout.tsx
+
+
+
+function RootLayout({ children  }) {
+    return /*#__PURE__*/ (0,jsx_runtime.jsxs)("html", {
+        lang: "en",
+        children: [
+            /*#__PURE__*/ jsx_runtime.jsx("head", {
+                children: /*#__PURE__*/ jsx_runtime.jsx(Head, {
+                    title: "Kings cup"
+                })
+            }),
+            /*#__PURE__*/ jsx_runtime.jsx("body", {
+                children: children
+            })
+        ]
+    });
+}
+
+
+/***/ }),
+
+/***/ 27306:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+/* __next_internal_client_entry_do_not_use__ */ const { createProxy  } = __webpack_require__(14353);
+module.exports = createProxy("/home/najah/kings_cup/src/app/page.tsx");
+
+
+/***/ }),
+
+/***/ 99859:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ Home)
+});
+
+// EXTERNAL MODULE: external "next/dist/compiled/react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(56786);
+// EXTERNAL MODULE: ./src/app/page.module.css
+var page_module = __webpack_require__(61159);
+var page_module_default = /*#__PURE__*/__webpack_require__.n(page_module);
+// EXTERNAL MODULE: ./node_modules/@mui/system/index.js
+var system = __webpack_require__(47447);
+// EXTERNAL MODULE: ./node_modules/next/image.js
+var next_image = __webpack_require__(48421);
+var image_default = /*#__PURE__*/__webpack_require__.n(next_image);
+// EXTERNAL MODULE: ./src/app/components/Kings/King/king.module.css
+var king_module = __webpack_require__(84315);
+var king_module_default = /*#__PURE__*/__webpack_require__.n(king_module);
+;// CONCATENATED MODULE: ./src/app/components/Kings/King/king.tsx
+
+
+
+const King = (props)=>{
+    return /*#__PURE__*/ jsx_runtime_.jsx(jsx_runtime_.Fragment, {
+        children: /*#__PURE__*/ jsx_runtime_.jsx("div", {
+            className: (king_module_default()).card,
+            children: /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                className: (king_module_default()).inner,
+                children: /*#__PURE__*/ jsx_runtime_.jsx((image_default()), {
+                    src: props.url,
+                    alt: "表面",
+                    width: 150,
+                    height: 300,
+                    priority: true,
+                    className: (king_module_default()).img
+                })
+            })
+        })
+    });
+};
+
+;// CONCATENATED MODULE: ./src/app/components/Kings/CrowbarKing/crowbarKing.tsx
+
+
+const CrowbarKing = ()=>{
+    return /*#__PURE__*/ jsx_runtime_.jsx(jsx_runtime_.Fragment, {
+        children: /*#__PURE__*/ jsx_runtime_.jsx(King, {
+            url: "/card-back-Blue.png"
+        })
+    });
+};
+
+;// CONCATENATED MODULE: ./src/app/components/FirstView/firstView.tsx
+
+
+const FirstView = ()=>{
+    return /*#__PURE__*/ jsx_runtime_.jsx(jsx_runtime_.Fragment, {
+        children: /*#__PURE__*/ jsx_runtime_.jsx(CrowbarKing, {})
+    });
+};
+
+// EXTERNAL MODULE: ./src/app/components/Table/table.module.css
+var table_module = __webpack_require__(74577);
+var table_module_default = /*#__PURE__*/__webpack_require__.n(table_module);
+;// CONCATENATED MODULE: ./src/app/components/Table/table.tsx
+
+
+
+
+const Table = ()=>{
+    return /*#__PURE__*/ jsx_runtime_.jsx(jsx_runtime_.Fragment, {
+        children: /*#__PURE__*/ jsx_runtime_.jsx(system.Box, {
+            className: (table_module_default()).table,
+            children: /*#__PURE__*/ jsx_runtime_.jsx(FirstView, {})
+        })
+    });
+};
+
+// EXTERNAL MODULE: ./node_modules/@mui/material/node/index.js
+var node = __webpack_require__(64085);
+// EXTERNAL MODULE: ./src/app/components/Title/title.module.css
+var title_module = __webpack_require__(26279);
+var title_module_default = /*#__PURE__*/__webpack_require__.n(title_module);
+;// CONCATENATED MODULE: ./src/app/components/Title/title.tsx
+
+
+
+
+const Title = ()=>{
+    return /*#__PURE__*/ jsx_runtime_.jsx(jsx_runtime_.Fragment, {
+        children: /*#__PURE__*/ jsx_runtime_.jsx(node.Typography, {
+            variant: "h1",
+            children: /*#__PURE__*/ jsx_runtime_.jsx(node.Box, {
+                className: (title_module_default()).h1,
+                children: "Kings Cup"
+            })
+        })
+    });
+};
+
+;// CONCATENATED MODULE: ./src/app/page.tsx
+
+
+
+
+
+
+function Home() {
+    return /*#__PURE__*/ jsx_runtime_.jsx("main", {
+        className: (page_module_default()).app,
+        children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)(system.Box, {
+            children: [
+                /*#__PURE__*/ jsx_runtime_.jsx(Title, {}),
+                /*#__PURE__*/ jsx_runtime_.jsx(Table, {})
+            ]
+        })
+    });
+}
+
+
+/***/ }),
+
+/***/ 75553:
+/***/ (() => {
+
+
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [238], () => (__webpack_exec__(75385)));
+module.exports = __webpack_exports__;
+
+})();
