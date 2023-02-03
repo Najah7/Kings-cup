e42ed4fb899c14e506849d25a9d7e11559226656
Najah7 <najahmatsuo7@gmail.com>
2023-02-03 16:57:50 +0900

    🏗️ rootにnext.jsのプロジェクトを移動

---
+++ src/app/components/FirstView/firstView.tsx
@@ -0,0 +1,15 @@
+import { Table } from "@mui/material"
+import { StatusDisplay } from "../StatusDisplay/StatusDisplay"
+import { Card } from "../Card/card"
+import { StartButton } from "../StartButton/startButton"
+import { King } from "../Kings/King/king"
+import { CrowbarKing } from "../Kings/CrowbarKing/crowbarKing"
+
+export const FirstView = () => {
+
+    return (
+        <>
+            <CrowbarKing></CrowbarKing>
+        </>
+    )
+}
