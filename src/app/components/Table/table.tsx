cf4118bddc20f1ca1e260e20f16a0a4b332baf94
Najah7 <najahmatsuo7@gmail.com>
2023-02-04 15:09:01 +0900

    ⚗️ 初期ページの表示を確認するときの変更

--- src/app/components/Table/table.tsx
+++ src/app/components/Table/table.tsx
@@ -1,6 +1,6 @@
 import { Card } from '../Card/card'
 import { Box } from '@mui/system'
-// import { FirstView } from '../FirstView/firstView'
+import { FirstView } from '../FirstView/firstView'
 import styles from './table.module.css'
 
 export const Table = () => {
@@ -8,8 +8,7 @@ export const Table = () => {
     return (
         <>        
         <Box className={styles.table}>
-            {/* <FirstView></FirstView> */}
-            <Card></Card>
+            <FirstView></FirstView>
         </Box>
         </>
     )
