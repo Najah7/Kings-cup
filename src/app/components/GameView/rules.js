8e030301a069fb2a1f7e0fc4be4e4f43587f1b6c
Najah7 <najahmatsuo7@gmail.com>
2023-02-07 19:54:06 +0900

    🚧 ゲームロジック用のコンポーネントを作成。 ひと段落ついたので、一度コミット

---
+++ src/app/components/GameView/rules.js
@@ -0,0 +1,372 @@
+export const CARDS =[
+    {
+        "type": "spade",
+        "num": 1,
+        "rule-discription": "右隣の人が飲むのを止めるまで飲み続ける。",
+        "img": '⚔️',
+    },
+    {
+        "type": "spade",
+        "num": 2,
+        "rule-discription": "カードを引いた人が2人指名。指名された２人が飲む。",
+        "img": '👥',
+    },
+    {
+        "type": "spade",
+        "num": 3,
+        "rule-discription": "自分一人で飲む。",
+        "img": '🍺',
+    },
+    {
+        "type": "spade",
+        "num": 4,
+        "rule-discription": "女子だけ飲む。",
+        "img": '🚺',
+    },
+        {
+        "type": "spade",
+        "num": 5,
+        "rule-discription": "カードを引いた人の両隣が飲む。",
+        "img": '🖖',
+    },
+    {
+        "type": "spade",
+        "num": 6,
+        "rule-discription": "男子だけ飲む。",
+        "img": '🚹',
+    },
+    {
+        "type": "spade",
+        "num": 7,
+        "rule-discription": "手あげるの一番遅かった人が飲む。（ニョッキゲーム的な感じ）",
+        "img": '🙋',
+    },
+    {
+        "type": "spade",
+        "num": 8,
+        "rule-discription": "相棒（相棒を決めれる。その人が飲むときは一緒に飲む必要がある。ゲームが終了まで継続（∞に続くだから８））",
+        "img": '🤝',
+    },
+    {
+        "type": "spade",
+        "num": 9,
+        "rule-discription": "好きな飲みゲーム指定。みんなでゲームをし、負けた人が飲む。",
+        "img": '🎮',
+    },
+    {
+            
+        "type": "spade",
+        "num": 10,
+        "rule-discription": "山手線ゲーム。負けた人が飲む。",
+        "img": '🚃',
+    },
+    {
+            
+        "type": "spade",
+        "num": 11,
+        "rule-discription": "他の数字の中から１つ選ぶ。選ばれた数字の罰ゲーム実行。",
+        "img": '❌',
+    },
+    {
+            
+        "type": "spade",
+        "num": 12,
+        "rule-discription": "クエスチョンマスター（引いた人以外は、このカード引いた人の質問に答えてはいけない。答えてしまったら、一杯飲む。次の人がクイーンを引くまで継続）",
+        "img": "❓",
+    },
+    {
+            
+        "type": "spade",
+        "num": 13,
+        "rule-discription": "パス（飲む必要なし）。4枚目出たらゲーム終了。しかし、4枚目の人は２杯飲む。",
+        "img": '👑',
+    },
+    {
+            
+        "type": "crowbar",
+        "num": 1,
+        "rule-discription": "右隣の人が飲むのを止めるまで飲み続ける。",
+        "img": '⚔️',
+    },
+    {
+            
+        "type": "crowbar",
+        "num": 2,
+        "rule-discription": "カードを引いた人が2人指名。指名された２人が飲む。",
+        "img": '👥',
+    },
+    {
+            
+        "type": "crowbar",
+        "num": 3,
+        "rule-discription": "自分一人で飲む。",
+        "img": '🍺',
+    },
+    {
+            
+        "type": "crowbar",
+        "num": 4,
+        "rule-discription": "女子だけ飲む。",
+        "img": '🚺',
+    },
+    {
+            
+        "type": "crowbar",
+        "num": 5,
+        "rule-discription": "カードを引いた人の両隣が飲む。",
+        "img": '🖖',
+    },
+    {
+            
+        "type": "crowbar",
+        "num": 6,
+        "rule-discription": "男子だけ飲む。",
+        "img": '🚹',
+    },
+    {
+            
+        "type": "crowbar",
+        "num": 7,
+        "rule-discription": "手あげるの一番遅かった人が飲む。（ニョッキゲーム的な感じ）",
+        "img": '🙋',
+    },
+    {
+            
+        "type": "crowbar",
+        "num": 8,
+        "rule-discription": "相棒（相棒を決めれる。その人が飲むときは一緒に飲む必要がある。ゲームが終了まで継続（∞に続くだから８））",
+        "img": '🤝',
+    },
+    {
+            
+        "type": "crowbar",
+        "num": 9,
+        "rule-discription": "好きな飲みゲーム指定。みんなでゲームをし、負けた人が飲む。",
+        "img": '🎮',
+    },
+    {
+            
+        "type": "crowbar",
+        "num": 10,
+        "rule-discription": "山手線ゲーム。負けた人が飲む。",
+        "img": '🚃',
+    },
+    {
+            
+        "type": "crowbar",
+        "num": 11,
+        "rule-discription": "他の数字の中から１つ選ぶ。選ばれた数字の罰ゲーム実行。",
+        "img": '❌',
+    },
+    {
+            
+        "type": "crowbar",
+        "num": 12,
+        "rule-discription": "クエスチョンマスター（引いた人以外は、このカード引いた人の質問に答えてはいけない。答えてしまったら、一杯飲む。次の人がクイーンを引くまで継続）",
+        "img": "❓",
+    },
+    {
+            
+        "type": "crowbar",
+        "num": 13,
+        "rule-discription": "パス（飲む必要なし）。4枚目出たらゲーム終了。しかし、4枚目の人は２杯飲む。",
+        "img": '👑',
+    },
+    {
+            
+        "type": "heart",
+        "num": 1,
+        "rule-discription": "右隣の人が飲むのを止めるまで飲み続ける。",
+        "img": '⚔️',
+    },
+    {
+            
+        "type": "heart",
+        "num": 2,
+        "rule-discription": "カードを引いた人が2人指名。指名された２人が飲む。",
+        "img": '👥',
+    },
+    {
+            
+        "type": "heart",
+        "num": 3,
+        "rule-discription": "自分一人で飲む。",
+        "img": '🍺',
+    },
+    {
+            
+        "type": "heart",
+        "num": 4,
+        "rule-discription": "女子だけ飲む。",
+        "img": '🚺',
+    },
+    {
+            
+        "type": "heart",
+        "num": 5,
+        "rule-discription": "カードを引いた人の両隣が飲む。",
+        "img": '🖖',
+    },
+    {
+            
+        "type": "heart",
+        "num": 6,
+        "rule-discription": "男子だけ飲む。",
+        "img": '🚹',
+    },
+    {
+            
+        "type": "heart",
+        "num": 7,
+        "rule-discription": "手あげるの一番遅かった人が飲む。（ニョッキゲーム的な感じ）",
+        "img": '🙋',
+    },
+    {
+            
+        "type": "heart",
+        "num": 8,
+        "rule-discription": "相棒（相棒を決めれる。その人が飲むときは一緒に飲む必要がある。ゲームが終了まで継続（∞に続くだから８））",
+        "img": '🤝',
+    },
+    {
+            
+        "type": "heart",
+        "num": 9,
+        "rule-discription": "好きな飲みゲーム指定。みんなでゲームをし、負けた人が飲む。",
+        "img": '🎮',
+    },
+    {
+            
+        "type": "heart",
+        "num": 10,
+        "rule-discription": "山手線ゲーム。負けた人が飲む。",
+        "img": '🚃',
+    },
+    {
+            
+        "type": "heart",
+        "num": 11,
+        "rule-discription": "他の数字の中から１つ選ぶ。選ばれた数字の罰ゲーム実行。",
+        "img": '❌',
+    },
+    {
+            
+        "type": "heart",
+        "num": 12,
+        "rule-discription": "クエスチョンマスター（引いた人以外は、このカード引いた人の質問に答えてはいけない。答えてしまったら、一杯飲む。次の人がクイーンを引くまで継続）",
+        "img": "❓",
+    },
+    {
+            
+        "type": "heart",
+        "num": 13,
+        "rule-discription": "パス（飲む必要なし）。4枚目出たらゲーム終了。しかし、4枚目の人は２杯飲む。",
+        "img": '👑',
+    },
+    {
+            
+        "type": "diamond",
+        "num": 1,
+        "rule-discription": "右隣の人が飲むのを止めるまで飲み続ける。",
+        "img": '⚔️',
+    },
+    {
+            
+        "type": "diamond",
+        "num": 2,
+        "rule-discription": "カードを引いた人が2人指名。指名された２人が飲む。",
+        "img": '👥',
+    },
+    {
+            
+        "type": "diamond",
+        "num": 3,
+        "rule-discription": "自分一人で飲む。",
+        "img": '🍺',
+    },
+    {
+            
+        "type": "diamond",
+        "num": 4,
+        "rule-discription": "女子だけ飲む。",
+        "img": '🚺',
+    },
+    {
+            
+        "type": "diamond",
+        "num": 5,
+        "rule-discription": "カードを引いた人の両隣が飲む。",
+        "img": '🖖',
+    },
+    {
+            
+        "type": "diamond",
+        "num": 6,
+        "rule-discription": "男子だけ飲む。",
+        "img": '🚹',
+    },
+    {
+            
+        "type": "diamond",
+        "num": 7,
+        "rule-discription": "手あげるの一番遅かった人が飲む。（ニョッキゲーム的な感じ）",
+        "img": '🙋',
+    },
+    {
+            
+        "type": "diamond",
+        "num": 8,
+        "rule-discription": "相棒（相棒を決めれる。その人が飲むときは一緒に飲む必要がある。ゲームが終了まで継続（∞に続くだから８））",
+        "img": '🤝',
+    },
+    {
+            
+        "type": "diamond",
+        "num": 9,
+        "rule-discription": "好きな飲みゲーム指定。みんなでゲームをし、負けた人が飲む。",
+        "img": '🎮',
+    },
+    {
+            
+        "type": "diamond",
+        "num": 10,
+        "rule-discription": "山手線ゲーム。負けた人が飲む。",
+        "img": '🚃',
+    },
+    {
+            
+        "type": "diamond",
+        "num": 11,
+        "rule-discription": "他の数字の中から１つ選ぶ。選ばれた数字の罰ゲーム実行。",
+        "img": '❌',
+    },
+    {
+            
+        "type": "diamond",
+        "num": 12,
+        "rule-discription": "クエスチョンマスター（引いた人以外は、このカード引いた人の質問に答えてはいけない。答えてしまったら、一杯飲む。次の人がクイーンを引くまで継続）",
+        "img": "❓",
+    },
+    {
+            
+        "type": "diamond",
+        "num": 13,
+        "rule-discription": "パス（飲む必要なし）。4枚目出たらゲーム終了。しかし、4枚目の人は２杯飲む。",
+        "img": '👑',
+    },
+    {
+            
+        "type":"joker",
+        "num":0,
+        "rule-discription":"２杯飲む",
+        "img": '🃏'
+    },
+    {
+            
+        "type":"joker",
+        "num":0,
+        "rule-discription":"２杯飲む",
+        "img": '🃏',
+        
+    },
+]
