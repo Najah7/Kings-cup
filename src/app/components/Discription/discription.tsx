27339a1a654e81a11a972a8c2491ad0d92a4d6d5
Najah7 <najahmatsuo7@gmail.com>
2023-02-07 19:50:34 +0900

    💄 説明コンポーネントにボタンを追加

--- src/app/components/Discription/discription.tsx
+++ src/app/components/Discription/discription.tsx
@@ -1,4 +1,5 @@
 import { style } from '@mui/system'
+import { NextButton } from '../Button/NextButton/nextButton'
 import styles from './discription.module.css'
 
 export const Discription = (props: any) => {
@@ -7,7 +8,7 @@ export const Discription = (props: any) => {
         <>
         <div className={styles.container}>
             <h3><span className={styles.emoji}>{props.emoji}</span><br/><span className={styles.discription}>{props.discription}</span></h3>
-            
+            <NextButton onClick={props.onClick}/>
         </div>
         </>
     )
