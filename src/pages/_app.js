0e751c1f8764078880bc27cf0b0c8b36e1973088
Najah7 <najahmatsuo7@gmail.com>
2023-02-12 08:26:43 +0900

    💄 UIの共通部分を定義

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,8 @@
 import '@/styles/globals.css'
+import { Layout } from '@/components/Layout/layout'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
 }
