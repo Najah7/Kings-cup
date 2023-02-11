f95334ea4cfcd39899810a151bb79496feb81dae
Najah7 <najahmatsuo7@gmail.com>
2023-02-12 08:49:58 +0900

    💄 プレイヤーを登録するページのボタンの配置を変更

--- src/components/PlayerPage/playerPage.jsx
+++ src/components/PlayerPage/playerPage.jsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
-import { StartButton } from "../Button/StartButton/startButton";
+import { StartButton } from "../Button/linkButton/linkButton";
 import { InputForm } from "../InputForm/inputForm";
 import { PlayerList } from "../PlayerList/playerList";
 import styles from "./playerPage.module.css"
 
 export const PlayerPage = () => {
     const [playerList, setPlayerList] = useState([]);
+
+    
   
     return (
       <div className={styles.container}>
         <InputForm playerList={playerList} setPlayerList={setPlayerList} />
-        <PlayerList playerList={playerList} setPlayerList={setPlayerList} />
         <StartButton/>
+        <PlayerList playerList={playerList} setPlayerList={setPlayerList} />
+        
       </div>
     );
   }
