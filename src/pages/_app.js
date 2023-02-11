966f39c8cd33d655585c96b6421cbb62d74b9980
Najah7 <najahmatsuo7@gmail.com>
2023-02-12 08:28:00 +0900

    ✨ ゲームに使う変数をコンテキストに保存（最上層で定義したので、プロジェクト全体で参照可能）

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,36 @@
 import '@/styles/globals.css'
 import { Layout } from '@/components/Layout/layout'
 
+import { createContext, useState } from 'react'
+
+export let GameInfo = createContext()
+
 export default function App({ Component, pageProps }) {
+
+  const [numCards, setNumCards] = useState(54)
+  const [numKings, setNumKings] = useState(0)
+  const [players, setPlayers] = useState([])
+  const [playerIndex, setPlayerIndex] = useState(0)
+
+  let gameInfo = {
+    numCards: numCards,
+    setNumCards: setNumCards,
+    numKings: numKings,
+    setNumKings: setNumKings,
+    players:players,
+    setPlayers: setPlayers,
+    playerIndex: playerIndex,
+    setPlayerIndex: setPlayerIndex,
+  }
+
+  
+
+  return (
+    <GameInfo.Provider value={gameInfo}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
+    </GameInfo.Provider>
+    
+  ) 
 }
