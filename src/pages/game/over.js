5d54ef535a6544b308e405551c489fca91b89775
Najah7 <najahmatsuo7@gmail.com>
2023-02-12 08:47:56 +0900

    ✨ ゲームが終了したとき用のページを作成

---
+++ src/pages/game/over.js
@@ -0,0 +1,14 @@
+import { LinkButton } from '@/components/Button/linkButton/linkButton'
+import React from 'react'
+
+const GameOver = () => {
+  return (
+    <>
+        <h1>Game Over</h1>
+    <LinkButton path='/' text='Play again'/>
+    </>
+    
+  )
+}
+
+export default GameOver
