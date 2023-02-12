6e6eb701be861fdf9666ec68dae729d36bd1ad42
Najah7 <najahmatsuo7@gmail.com>
2023-02-12 09:11:21 +0900

    🔥 要らないファイルを削除

--- src/pages/game/diamond/[num]/index.js
+++ src/pages/game/diamond/[num]/index.js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import { useContext, useEffect, useState } from 'react';
-import { Card } from '../../../../components/Card/card';
-import { StatusDisplay } from '../../../../components/StatusDisplay/StatusDisplay';
+import { Card } from '@/components/Card/card';
+import { StatusDisplay } from '@/components/StatusDisplay/StatusDisplay';
 import { LinkButton } from '@/components/Button/linkButton/linkButton';
 import { GameInfo } from '@/pages/_app';
 import { UpdateToNextIndex } from 'helper/game';
